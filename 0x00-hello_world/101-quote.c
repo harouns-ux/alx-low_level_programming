bf03b06ed2bf27b81c28a729dea78490624b3c89
harouns-ux <fahadharoun1@gmail.com>
2022-12-08 09:26:12 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio,h>
+#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Entry
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
 
