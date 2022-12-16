5ae7b9b2e5f00ccd5d10aa0f68af1920cda07806
harouns-ux <fahadharoun1@gmail.com>
2022-12-15 20:16:32 -0800

    3-print_remaining_days.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,16 +1,12 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * main - Test function for positive or negative
- * Return: 0
- */
+#include <stdio.h>
 
-int main(void)
-{
-	int i;
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+void print_times_table(int n);
 
-	i = 0;
-	positive_or_negative(i);
-
-	return (0);
-}
+#endif
